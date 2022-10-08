0ad18d540fa82294d5e5b75f147700142b3b898e
Facundo Mastricchio <facundomastricchio@gmail.com>
2022-10-07 21:55:39 -0300

    Add resources alert

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,29 @@ use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
 
-const MAX_DISPENSERS: usize = 3;
+const MAX_DISPENSERS: u64 = 3;
+const INITIAL_MILK_FOAM: u64 = 100;
+const INITIAL_GROUND_COFFEE_BEANS: u64 = 100;
+const RESOURCE_ALERT_THRESHOLD: f64 = 0.5;
+
 
 fn main() {
-    let coffee_beans_to_grind = Arc::new(Mutex::new(100000));
-    let ground_coffee_beans = Arc::new((Mutex::new(10), Condvar::new()));
-    let milk_foam = Arc::new((Mutex::new(10), Condvar::new()));
-    let cold_milk = Arc::new(Mutex::new(100000));
+
+    let coffee_beans_to_grind_container = Arc::new(Mutex::new(100000));
+    let cold_milk_container = Arc::new(Mutex::new(100000));
+
+    let ground_coffee_beans_container = Arc::new((Mutex::new(INITIAL_GROUND_COFFEE_BEANS), Condvar::new()));
+    let milk_foam_container = Arc::new((Mutex::new(INITIAL_MILK_FOAM), Condvar::new()));
+
 
     let dispensers: Vec<JoinHandle<()>> = (1..MAX_DISPENSERS + 1)
         .map(|i| {
-            let ground_coffee_beans_clone = ground_coffee_beans.clone();
-            let milk_foam_clone = milk_foam.clone();
+            let ground_coffee_beans_clone = ground_coffee_beans_container.clone();
+            let milk_foam_clone = milk_foam_container.clone();
             thread::spawn(move || loop {
-                let coffee_order = rand::random::<u64>() % 10;
-                let milk_order = rand::random::<u64>() % 10;
-                let water_order = rand::random::<u64>() % 10;
+                let coffee_order = rand::random::<u64>() % 5;
+                let milk_order = rand::random::<u64>() % 5;
+                let water_order = rand::random::<u64>() % 5;
                 println!("[Dispenser {}]: Recibió orden de café con: {} gr de café, {} gr de leche y {} gr de agua", i, coffee_order, milk_order, water_order);
                 if coffee_order > 0 {
                     let (lock, cvar) = &*ground_coffee_beans_clone;
@@ -62,8 +69,9 @@ fn main() {
         })
         .collect();
 
-    let cold_milk_clone = cold_milk.clone();
-    let milk_foam_clone = milk_foam.clone();
+    let cold_milk_clone = cold_milk_container.clone();
+    let milk_foam_clone = milk_foam_container.clone();
+
     let milk_refill = thread::spawn(move || loop {
         let (lock, cvar) = &*milk_foam_clone;
         let mut milk_foam = cvar
@@ -82,8 +90,9 @@ fn main() {
         cvar.notify_all();
     });
 
+    let ground_coffee_beans_container_clone = ground_coffee_beans_container.clone();
     let coffee_refill = thread::spawn(move || loop {
-        let (lock, cvar) = &*ground_coffee_beans;
+        let (lock, cvar) = &*ground_coffee_beans_container_clone;
         let mut ground_coffee_beans = cvar
             .wait_while(lock.lock().unwrap(), |&mut ground_coffee_beans| {
                 ground_coffee_beans > 0
@@ -95,15 +104,53 @@ fn main() {
             value_to_refill
         );
         thread::sleep(Duration::from_millis(100));
-        let mut coffee_beans_to_grind = coffee_beans_to_grind.lock().unwrap();
+        let mut coffee_beans_to_grind = coffee_beans_to_grind_container.lock().unwrap();
         *coffee_beans_to_grind -= value_to_refill;
         *ground_coffee_beans += value_to_refill;
         println!("[Refill de café]: Terminó de convertir granos de café");
         cvar.notify_all();
     });
 
+
+
+    let ground_coffee_beans_clone = ground_coffee_beans_container.clone();
+    let alert_system_for_coffee_beans = thread::spawn(move || {
+        loop {
+            let (lock, cvar) = &*ground_coffee_beans_clone;
+            let ground_coffee_beans = cvar
+                .wait_while(lock.lock().unwrap(), |&mut ground_coffee_beans| {
+                    ground_coffee_beans > (INITIAL_GROUND_COFFEE_BEANS as f64 * RESOURCE_ALERT_THRESHOLD) as u64
+                })
+                .unwrap();
+            let
+            println!(
+                "[Alerta de recursos]: El nivel de granos de café es de {} ({}% del total inicial, threshold de {}%)",
+                *ground_coffee_beans, (*ground_coffee_beans / INITIAL_GROUND_COFFEE_BEANS) * 100, RESOURCE_ALERT_THRESHOLD * 100.0
+            );
+        }
+    });
+
+    let milk_foam_container_clone = milk_foam_container.clone();
+    let alert_system_for_milk = thread::spawn(move || {
+        loop {
+            let (lock, cvar) = &*milk_foam_container_clone;
+            let milk_foam = cvar
+                .wait_while(lock.lock().unwrap(), |&mut milk_foam| {
+                    milk_foam > (INITIAL_MILK_FOAM as f64 * RESOURCE_ALERT_THRESHOLD) as u64
+                })
+                .unwrap();
+            println!(
+                "[Alerta de recursos]: El nivel de leche espumada es de {} ({}% del total inicial, threshold de {}%)",
+                *milk_foam, (*milk_foam / INITIAL_MILK_FOAM) * 100, RESOURCE_ALERT_THRESHOLD * 100.0
+            );
+        }
+    });
+
     coffee_refill.join().unwrap();
     milk_refill.join().unwrap();
+    alert_system_for_coffee_beans.join().unwrap();
+    alert_system_for_milk.join().unwrap();
+
 
     let _: Vec<()> = dispensers
         .into_iter()
